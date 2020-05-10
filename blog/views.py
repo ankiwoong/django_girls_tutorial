b8e0ba001604ccfc34112d36fdfacf5e957b344e
AnKiWoong <ankiwoong@gmail.com>
2020-05-10 10:52:44 +0900

    Modify view & Edit file name

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
+
 from .models import Post
+from .forms import PostForm
 
 
 def post_list(request):
@@ -13,3 +15,8 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
